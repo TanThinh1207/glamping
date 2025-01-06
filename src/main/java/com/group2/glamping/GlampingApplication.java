b8d42a0b44a657017e1dcaaeda223e8f33b9fbbe
Tan Thinh Hua <thinhhtse183224@fpt.edu.vn>
2025-01-06 22:06:40 +0700

    add project

---
+++ src/main/java/com/group2/glamping/GlampingApplication.java
@@ -0,0 +1,13 @@
+package com.group2.glamping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlampingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlampingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group2/glamping/GlampingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group2.glamping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlampingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
