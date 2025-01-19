04dda12ee8384ed8c17a125942705ee092e033ed
BinhNCTSE183284 <binhnctse183284@fpt.edu.vn>
2025-01-19 22:25:23 +0700

    refactor: move package

--- src/main/java/com/group2/glamping/model/entity/Booking.java
+++ src/main/java/com/group2/glamping/model/entity/Booking.java
@@ -1,13 +1,11 @@
 package com.group2.glamping.model.entity;
 
-import com.group2.glamping.user.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
--- src/main/java/com/group2/glamping/model/entity/CampSite.java
+++ src/main/java/com/group2/glamping/model/entity/CampSite.java
@@ -1,6 +1,5 @@
 package com.group2.glamping.model.entity;
 
-import com.group2.glamping.user.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
