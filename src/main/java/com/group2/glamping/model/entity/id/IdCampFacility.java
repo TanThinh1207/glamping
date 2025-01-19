c2f342ba8a65c3564f8415ab77545fb83920a54c
BinhNCTSE183284 <binhnctse183284@fpt.edu.vn>
2025-01-19 23:02:53 +0700

    refactor: add @Column

--- src/main/java/com/group2/glamping/model/entity/id/IdCampFacility.java
+++ src/main/java/com/group2/glamping/model/entity/id/IdCampFacility.java
@@ -1,13 +1,25 @@
 package com.group2.glamping.model.entity.id;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class IdCampFacility implements Serializable {
 
+    @Column(name = "id_camp")
     private int campId;
+
+    @Column(name = "id_facility")
     private int facilityId;
 }
--- src/main/java/com/group2/glamping/model/entity/id/IdCampSiteUtility.java
+++ src/main/java/com/group2/glamping/model/entity/id/IdCampSiteUtility.java
@@ -1,13 +1,25 @@
 package com.group2.glamping.model.entity.id;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class IdCampSiteUtility implements Serializable {
 
+    @Column(name = "id_camp_site")
     private int campSiteId;
+
+    @Column(name = "id_utility")
     private int utilityId;
 }
