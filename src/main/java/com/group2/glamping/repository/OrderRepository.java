0edee077401f4a07defb0ebcc3abc169a0ddf868
Nguyen Tran Tuan Khang <khangnttse183039@fpt.edu.vn>
2025-01-21 21:43:23 +0700

    Update Entity 5

---
+++ src/main/java/com/group2/glamping/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.group2.glamping.repository;
+
+import com.group2.glamping.model.entity.Image;
+import com.group2.glamping.model.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+}
