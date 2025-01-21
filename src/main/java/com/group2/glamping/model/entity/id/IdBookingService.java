5154fc2444c471a555e4abacfe67cb3c68e0d1fe
Nguyen Tran Tuan Khang <khangnttse183039@fpt.edu.vn>
2025-01-21 21:44:24 +0700

    Add Serializable

--- src/main/java/com/group2/glamping/model/entity/id/IdBookingService.java
+++ src/main/java/com/group2/glamping/model/entity/id/IdBookingService.java
@@ -2,8 +2,10 @@ package com.group2.glamping.model.entity.id;
 
 import jakarta.persistence.Embeddable;
 
+import java.io.Serializable;
+
 @Embeddable
-public class IdBookingService {
+public class IdBookingService implements Serializable {
 
     private int bookingId;
     private int serviceId;
--- src/main/java/com/group2/glamping/model/entity/id/IdCampTypeFacility.java
+++ src/main/java/com/group2/glamping/model/entity/id/IdCampTypeFacility.java
@@ -2,8 +2,10 @@ package com.group2.glamping.model.entity.id;
 
 import jakarta.persistence.Embeddable;
 
+import java.io.Serializable;
+
 @Embeddable
-public class IdCampTypeFacility {
+public class IdCampTypeFacility implements Serializable {
 
     private int campTypeId;
     private int facilityId;
