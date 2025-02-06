817f84a902fcfd077d4e1a49cb7e9b63bf25a872
BinhNCTSE183284 <binhnctse183284@fpt.edu.vn>
2025-02-06 14:28:51 +0700

    refactor: catch exception

--- src/main/java/com/group2/glamping/auth/AuthenticationController.java
+++ src/main/java/com/group2/glamping/auth/AuthenticationController.java
@@ -1,9 +1,11 @@
 package com.group2.glamping.auth;
 
+import com.group2.glamping.exception.AppException;
 import com.group2.glamping.model.dto.requests.AuthenticationRequest;
 import com.group2.glamping.model.dto.requests.RegisterRequest;
-import com.group2.glamping.model.dto.response.AuthenticationResponse;
+import com.group2.glamping.model.dto.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,18 +18,43 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(
+    public ResponseEntity<BaseResponse> register(
             @RequestBody RegisterRequest request
     ) {
-        return ResponseEntity.ok(authenticationService.register(request));
+        try {
+            return ResponseEntity.ok(BaseResponse.builder()
+                    .data(authenticationService.register(request))
+                    .message("Register successfully")
+                    .statusCode(HttpStatus.OK.value())
+                    .build());
+        } catch (AppException e) {
+            return ResponseEntity.badRequest().body(BaseResponse.builder()
+                    .statusCode(e.getErrorCode().getCode())
+                    .message(e.getMessage())
+                    .build());
+        }
+
     }
 
 
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> authenticate(
+    public ResponseEntity<BaseResponse> authenticate(
             @RequestBody AuthenticationRequest request
     ) {
-        return ResponseEntity.ok(authenticationService.authenticate(request));
+
+        try {
+            return ResponseEntity.ok(BaseResponse.builder()
+                    .message("Authenticate successfully")
+                    .statusCode(HttpStatus.OK.value())
+                    .data(authenticationService.authenticate(request))
+                    .build());
+        } catch (AppException e) {
+            return ResponseEntity.badRequest().body(BaseResponse.builder()
+                    .statusCode(e.getErrorCode().getCode())
+                    .message(e.getMessage())
+                    .build());
+        }
     }
+
 
 }
--- src/main/java/com/group2/glamping/exception/ErrorCode.java
+++ src/main/java/com/group2/glamping/exception/ErrorCode.java
@@ -15,7 +15,8 @@ public enum ErrorCode {
     UNAUTHENTICATED(1006, "Unauthenticated", HttpStatus.UNAUTHORIZED),
     UNAUTHORIZED(1007, "You do not have permission", HttpStatus.FORBIDDEN),
     WRONG_PASSWORD(1008, "Wrong password", HttpStatus.BAD_REQUEST),
-    INVALID_DOB(1009, "Your age must be at least {min}", HttpStatus.BAD_REQUEST),
+    INVALID_DOB(1009, "Your age must be at least 16", HttpStatus.BAD_REQUEST),
+
     ;
 
     ErrorCode(int code, String message, HttpStatusCode statusCode) {
